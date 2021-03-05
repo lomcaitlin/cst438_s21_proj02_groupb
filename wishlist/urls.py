df15d64a4dc4b806e4dd224953cd96879d6913a0
synkae <synkae123@gmail.com>
2021-03-05 14:25:17 -0800

    Added a view to see all existing users
    -Added some test users into the DB
    -Added a view to see all existing users
    
    TODO:
    - add functionality to change user password and admin status

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-	path('wishlistApp/', include('wishlistApp.urls')),
+	path('', include('wishlistApp.urls')),
     path('admin/', admin.site.urls),
 ]
--- wishlistApp/admin.py
+++ wishlistApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, URL, Item
+
+admin.site.register(User)
+admin.site.register(URL)
+admin.site.register(Item)
