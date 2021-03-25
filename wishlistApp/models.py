891d75d66d5329827574d17040d5d4320e95626f
lomcaitlin <lomcaitlin@gmail.com>
2021-03-25 12:26:54 -0700

    Update priority to only accept values from 0-5

--- wishlistApp/models.py
+++ wishlistApp/models.py
@@ -8,6 +8,15 @@ class User(models.Model):
     admin = models.BooleanField(default=False)
 """
 
+class IntegerRangeField(models.IntegerField):
+    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
+        defaults.update(kwargs)
+        return super(IntegerRangeField, self).formfield(**defaults)
+
 class URL(models.Model):
     url = models.CharField(max_length=500)
 
@@ -15,6 +24,6 @@ class Item(models.Model):
     name = models.CharField(max_length=200)
     image = models.CharField(max_length=500)
     description = models.CharField(max_length=1000)
-    priority = models.IntegerField(default=0)
+    priority = IntegerRangeField(min_value=0, max_value=5)
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     url_id = models.ForeignKey(URL, on_delete=models.CASCADE)
